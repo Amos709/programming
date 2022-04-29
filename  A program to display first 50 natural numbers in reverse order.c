582400a67db362104d0e537b6d90de3b899cfafc
Amos709 <102530531+Amos709@users.noreply.github.com>
2022-04-29 11:45:48 +0300

    Add files via upload

---
+++  A program to display first 50 natural numbers in reverse order.c
@@ -0,0 +1,13 @@
+// A program to display first 50 natural numbers in reverse order
+#include<stdio.h>
+int main ()
+{
+int k;
+printf("the first 50 numbers using  for loop\n");
+for(k=50;k>0;k--)
+{
+printf("%d\n",k);
+}
+printf("end of the loop");
+return 0;
+}
