4f3a177043b9b6db07b20d9a780025b600f5e447
Amos709 <102530531+Amos709@users.noreply.github.com>
2022-04-29 11:50:17 +0300

    Add files via upload

---
+++ program to demonstrate usage of arith.etic operators +-__%.c
@@ -0,0 +1,20 @@
+//a program to demonstrate usage of arithmetic operator+,-,*,/,%
+#include<stdio.h>
+
+int main()
+{
+int a=10,b=2;
+int  addition,subtraction,multiplication,division,modulus;
+addition=a+b;
+subtraction=a-b;
+multiplication=a*b;
+division=a/b;
+modulus=a%b;
+    printf(" addition of two numbers a,b is %d\n",addition);
+    printf(" subtraction of two numbers a,b is %d\n",subtraction);
+    printf(" multiplication of two numbers a,b is %d\n",multiplication);
+    printf("division of two numbers a,b is %d\n",division);
+    printf(" modulus of two numbers a,b is %d\n",modulus);
+    return 0;
+    
+}
