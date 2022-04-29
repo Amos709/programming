4377e16bfacbc7a182f16d9647a88e816566ca50
Amos709 <102530531+Amos709@users.noreply.github.com>
2022-04-29 11:48:16 +0300

    Add files via upload

---
+++ c program to find sum of (1 to 10 ) by for loop.c
@@ -0,0 +1,11 @@
+//A program to find sum of (1 to 10) by for loop method
+#include<stdio.h>
+
+int main()
+{
+    int x,sum=0;x;
+    for(x=1; x<=10; x++)
+    sum=sum+x;
+    printf("sum of 1 to 10 is %d\n",sum);
+    return 0;
+}
